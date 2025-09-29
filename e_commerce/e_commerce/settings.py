7589e45fc2adf46989cc29651860ea3c6cff575c
Tasleem <mullatasleem25@example.com>
2025-09-29 13:40:00 +0530

    Prepare project for Render deployment

--- e_commerce/e_commerce/settings.py
+++ e_commerce/e_commerce/settings.py
@@ -127,6 +127,9 @@ LOGIN_URL = '/login/'
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
